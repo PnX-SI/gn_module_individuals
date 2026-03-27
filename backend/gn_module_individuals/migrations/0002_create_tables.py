c8ac8d10153d85cd7bf87452cc12f08429593c6a
CynthiaBorotPNV <cynthia.borot@vanoise-parcnational.fr>
2026-03-27 17:39:52 +0100

    feat: Change migration file on nomenclatures

--- backend/gn_module_individuals/migrations/0002_create_tables.py
+++ backend/gn_module_individuals/migrations/0002_create_tables.py
@@ -7,7 +7,7 @@ Create Date: 2026-03-19 15:40:48.398854
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.sql import select, table, column, functions as func
 
 # revision identifiers, used by Alembic.
 revision = '0002_create_tables'
@@ -39,7 +39,7 @@ def upgrade():
             sa.Text(),
             nullable=True,
         ),
-      sa.Column(
+        sa.Column(
             "provider_device_id",
             sa.Text(),
             nullable=True,
@@ -73,6 +73,7 @@ def upgrade():
         ),
         schema=SCHEMA_NAME,
     )
+
     op.execute(f"""
         CREATE OR REPLACE FUNCTION {SCHEMA_NAME}.set_meta_dates()
         RETURNS TRIGGER AS $$
@@ -96,30 +97,117 @@ def upgrade():
         EXECUTE FUNCTION {SCHEMA_NAME}.set_meta_dates();
     """)
 
+    bib_nomencl = table(
+        "bib_nomenclatures_types",
+        column("id_type", sa.Integer),
+        column("mnemonique", sa.String),
+        column("label_default", sa.String),
+        column("definition_default", sa.String),
+        column("label_fr", sa.String),
+        column("definition_fr", sa.String),
+        column("source", sa.String),
+        schema="ref_nomenclatures"
+    )
+
+    req_obj = select(func.max(bib_nomencl.c.id_type))
+    last_value = conn.execute(req_obj).scalar()
+    next_id = (last_value or 0) + 1
+    
     op.execute(
-        sa.text(
-            f"""
-            INSERT INTO ref_nomenclatures.bib_nomenclatures_types (id_type,mnemonique,label_default,definition_default,label_fr,definition_fr,source) 
-            VALUES (129,'TYPE_CAPTEUR', 'Type de capteur de position', 'Capteur de position','Type de capteur de position', 'Capteur de position', 'GEONATURE')  
-            """
+        sa.insert(bib_nomencl).values(
+            [
+                {
+                    "id_type": next_id,
+                    "mnemonique": 'TYPE_DISPO_SUIVI',
+                    "label_default": 'Type de dispositif de suivi',
+                    "definition_default": 'Dispositif de suivi',
+                    "label_fr": 'Type de dispositif de suivi',
+                    "definition_fr": 'Dispositif de suivi',
+                    "source": 'GEONATURE',
+                },
+            ]
         )
     )
+
+    t_nomencl = table(
+        "t_nomenclatures",
+        column("id_type", sa.Integer),
+        column("cd_nomenclature", sa.String),
+        column("mnemonique", sa.String),
+        column("label_default", sa.String),
+        column("definition_default", sa.String),
+        column("label_fr", sa.String),
+        column("definition_fr", sa.String),
+        column("source", sa.String),
+        column("hierarchy", sa.String),
+        column("active", sa.Boolean),
+        schema="ref_nomenclatures"
+    )
+
     op.execute(
-        sa.text(
-            f"""
-            INSERT INTO ref_nomenclatures.t_nomenclatures (id_type,	cd_nomenclature,mnemonique,label_default,definition_default,label_fr,definition_fr,
-            source,	hierarchy,active)
-            VALUES (129,1,'GPS','Balise GPS','Balise GPS','Balise GPS','Balise GPS','GEONATURE','129.001',true), 
-            (129,2,'GSM','Balise GSM','Balise GSM (réseau mobile)','Balise GSM','Balise GSM (réseau mobile)','GEONATURE','129.002',true), 
-            (129,3,'Argos','Balise Argos','Balise Argos','Balise Argos','Balise Argos','GEONATURE','129.003',true),
-            (129,4,'VHF','Balise VHF','Balise VHF','Balise VHF','Balise VHF','GEONATURE','129.004',true)
-            """
+        sa.insert(t_nomencl).values(
+            [
+                {
+                    "id_type": next_id,
+                    "cd_nomenclature": 1,
+                    "mnemonique": 'GPS',
+                    "label_default": 'Balise GPS',
+                    "definition_default": 'Balise GPS',
+                    "label_fr": 'Balise GPS',
+                    "definition_fr": 'Balise GPS',
+                    "source": 'GEONATURE',
+                    "hierarchy": '129.001',
+                    "active": True
+                },
+                {
+                    "id_type": next_id,
+                    "cd_nomenclature": 2,
+                    "mnemonique": 'GSM',
+                    "label_default": 'Balise GSM',
+                    "definition_default": 'Balise GSM',
+                    "label_fr": 'Balise GSM',
+                    "definition_fr": 'Balise GSM',
+                    "source": 'GEONATURE',
+                    "hierarchy": '129.001',
+                    "active": True
+                },
+                {
+                    "id_type": next_id,
+                    "cd_nomenclature": 3,
+                    "mnemonique": 'Argos',
+                    "label_default": 'Balise Argos',
+                    "definition_default": 'Balise Argos',
+                    "label_fr": 'Balise Argos',
+                    "definition_fr": 'Balise Argos',
+                    "source": 'GEONATURE',
+                    "hierarchy": '129.001',
+                    "active": True
+                },
+                {
+                    "id_type": next_id,
+                    "cd_nomenclature": 4,
+                    "mnemonique": 'VHF',
+                    "label_default": 'Balise VHF',
+                    "definition_default": 'Balise VHF',
+                    "label_fr": 'Balise VHF',
+                    "definition_fr": 'Balise VHF',
+                    "source": 'GEONATURE',
+                    "hierarchy": '129.001',
+                    "active": True
+                },
+            ]
         )
     )
 
 def downgrade():
     op.execute(f"DROP TRIGGER IF EXISTS tr_meta_dates_bib_tracking_devices ON {SCHEMA_NAME}.bib_tracking_devices;")
     op.execute(f"DROP FUNCTION IF EXISTS {SCHEMA_NAME}.set_meta_dates();")
-    op.execute(f"DELETE FROM ref_nomenclatures.t_nomenclatures WHERE id_type = 129;")
-    op.execute(f"DELETE FROM ref_nomenclatures.bib_nomenclatures_types WHERE id_type = 129;")
+    op.execute(
+        f"""DELETE FROM ref_nomenclatures.t_nomenclatures t
+            USING ref_nomenclatures.bib_nomenclatures_types b
+            WHERE t.id_type = b.id_type
+            AND b.mnemonique = 'TYPE_DISPO_SUIVI';
+        """
+    )
+    op.execute(f"DELETE FROM ref_nomenclatures.bib_nomenclatures_types WHERE mnemonique = 'TYPE_DISPO_SUIVI';")
     op.drop_table("bib_tracking_devices", schema=SCHEMA_NAME)
