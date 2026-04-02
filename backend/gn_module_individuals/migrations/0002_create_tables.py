4b18f19c1cf2a9ee33d075982e36236a80e638a1
Lagaye Claire <claire.lagaye@vanoise-parcnational.fr>
2026-04-02 15:43:51 +0200

    feat: Alembic revisions update for t_individual_deployments, table creatio, test data insert

--- backend/gn_module_individuals/migrations/0002_create_tables.py
+++ backend/gn_module_individuals/migrations/0002_create_tables.py
@@ -74,16 +74,97 @@ def upgrade():
         schema=SCHEMA_NAME,
     )
 
+    t_individual_deployments = op.create_table(
+        "t_individual_deployments",
+        sa.Column(
+            "id_deployment",
+            sa.Integer(),
+            primary_key=True,
+            autoincrement=True,
+        ),
+        sa.Column(
+            "id_capture",
+            sa.Integer(),
+            # sa.ForeignKey("t_captures.id_capture"),
+            nullable=False,
+        ),
+        sa.Column(
+            "id_individual",
+            sa.Integer(),
+            sa.ForeignKey("gn_monitoring.t_individuals.id_individual"),
+            nullable=False,
+        ),
+        sa.Column(
+            "id_nomenclature_deployment_type",
+            sa.Integer(),
+            sa.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature"),
+            nullable=True,
+        ),
+        sa.Column(
+            "id_nomenclature_deployment_location",
+            sa.Integer(),
+            sa.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature"),
+            nullable=True,
+        ),
+        sa.Column(
+            "id_tracking_device",
+            sa.Integer(),
+            sa.ForeignKey("gn_individual.bib_tracking_devices.id_tracking_device"),
+            nullable=False,
+        ),
+        sa.Column(
+            "marking_code",
+            sa.Text(),
+            nullable=True,
+        ),
+        sa.Column(
+            "install_date",
+            sa.DateTime(),
+            nullable=False,
+        ),
+        sa.Column(
+            "removal_date",
+            sa.DateTime(),
+            nullable=True,
+        ),
+        sa.Column(
+            "comment",
+            sa.Text(),
+            nullable=True,
+        ),
+        sa.Column(
+            "additional_data",
+            sa.JSON(),
+            nullable=True,
+        ),
+        sa.Column(
+            "id_digitiser",
+            sa.Integer(),
+            sa.ForeignKey("utilisateurs.t_roles.id_role"),
+            nullable=True,
+        ),
+        sa.Column(
+            "meta_create_date",
+            sa.DateTime(),
+            nullable=True,
+        ),
+        sa.Column(
+            "meta_update_date",
+            sa.DateTime(),
+            nullable=True,
+        ),
+        schema=SCHEMA_NAME,
+    )
+
+
     op.execute(f"""
         CREATE OR REPLACE FUNCTION {SCHEMA_NAME}.set_meta_dates()
         RETURNS TRIGGER AS $$
         BEGIN
             IF TG_OP = 'INSERT' THEN
                 NEW.meta_create_date := NOW();
-                NEW.meta_update_date := NOW();
             ELSIF TG_OP = 'UPDATE' THEN
                 NEW.meta_update_date := NOW();
-                NEW.meta_create_date := OLD.meta_create_date;
             END IF;
             RETURN NEW;
         END;
@@ -93,6 +174,13 @@ def upgrade():
     op.execute(f"""
         CREATE TRIGGER tr_meta_dates_bib_tracking_devices
         BEFORE INSERT OR UPDATE ON {SCHEMA_NAME}.bib_tracking_devices
+        FOR EACH ROW
+        EXECUTE FUNCTION {SCHEMA_NAME}.set_meta_dates();
+    """)
+
+    op.execute(f"""
+        CREATE TRIGGER tr_meta_dates_individual_deployments
+        BEFORE INSERT OR UPDATE ON {SCHEMA_NAME}.t_individual_deployments
         FOR EACH ROW
         EXECUTE FUNCTION {SCHEMA_NAME}.set_meta_dates();
     """)
@@ -201,6 +289,7 @@ def upgrade():
 
 def downgrade():
     op.execute(f"DROP TRIGGER IF EXISTS tr_meta_dates_bib_tracking_devices ON {SCHEMA_NAME}.bib_tracking_devices;")
+    op.execute(f"DROP TRIGGER IF EXISTS tr_meta_dates_individual_deployments ON {SCHEMA_NAME}.t_individual_deployments;")
     op.execute(f"DROP FUNCTION IF EXISTS {SCHEMA_NAME}.set_meta_dates();")
     op.execute(
         f"""DELETE FROM ref_nomenclatures.t_nomenclatures t
@@ -210,4 +299,5 @@ def downgrade():
         """
     )
     op.execute(f"DELETE FROM ref_nomenclatures.bib_nomenclatures_types WHERE mnemonique = 'TYPE_DISPO_SUIVI';")
-    op.drop_table("bib_tracking_devices", schema=SCHEMA_NAME)
+    op.drop_table("t_individual_deployments", schema=SCHEMA_NAME,if_exists=True)
+    op.drop_table("bib_tracking_devices", schema=SCHEMA_NAME,if_exists=True)
