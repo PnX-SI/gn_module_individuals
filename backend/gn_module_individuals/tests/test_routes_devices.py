9b1e65e430979f3188317bffdb96e6c01075cc15
Lagaye Claire <claire.lagaye@vanoise-parcnational.fr>
2026-04-03 17:02:05 +0200

    fix: tests backend without pagination

--- backend/gn_module_individuals/tests/test_routes_devices.py
+++ backend/gn_module_individuals/tests/test_routes_devices.py
@@ -2,23 +2,30 @@ from flask import url_for
 from pypnusershub.tests.utils import logged_user
 
 
-def test_list_devices_returns_paginated_list(client, users):
+# def test_list_devices_returns_paginated_list(client, users):
+#     with logged_user(client, users["admin_user"]):
+#         response = client.get(url_for("individuals.list_devices",page=1, per_page=10))
+#     payload = response.get_json()
+#     assert response.status_code == 200
+#     assert isinstance(payload, dict)
+#     assert payload.get("items") is not None
+#     assert isinstance(payload.get("items"), list)
+#     assert payload.get("total") is not None
+#     assert payload.get("page") is not None
+#     assert payload.get("per_page") is not None
+#     assert payload.get("pages") is not None
+#     assert payload.get("has_next") is not None
+#     assert payload.get("has_prev") is not None
+#     assert payload.get("prev_num") is not None
+#     assert payload.get("next_num") is not None
+
+def test_list_devices_without_pagination(client, users):
     with logged_user(client, users["admin_user"]):
         response = client.get(url_for("individuals.list_devices"))
 
     payload = response.get_json()
     assert response.status_code == 200
-    assert isinstance(payload, dict)
-    assert payload.get("items") is not None
-    assert isinstance(payload.get("items"), list)
-    assert payload.get("pagination").get("total") is not None
-    assert payload.get("pagination").get("page") is not None
-    assert payload.get("pagination").get("per_page") is not None
-    assert payload.get("pagination").get("pages") is not None
-    assert "has_next" in payload.get("pagination")
-    assert "has_prev" in payload.get("pagination")
-    assert "prev_num" in payload.get("pagination")
-    assert "next_num" in payload.get("pagination")
+    assert isinstance(payload, list)
 
 def test_get_device_not_found(client, users):
     with logged_user(client, users["admin_user"]):
@@ -40,3 +47,15 @@ def test_get_devices_filtering(client, users, devices):
     with logged_user(client, users["admin_user"]):
         response = client.get(url_for("individuals.list_devices", per_page=1))
     payload = response.get_json()
+
+def test_device_fields(client, users):
+    with logged_user(client, users["admin_user"]):
+        response = client.get(
+            url_for("individuals.list_devices", page=1, per_page=1)
+        )
+
+    payload = response.get_json()
+    item = payload["items"][0]
+
+    assert "id_tracking_device" in item
+    assert "provider_name" in item
