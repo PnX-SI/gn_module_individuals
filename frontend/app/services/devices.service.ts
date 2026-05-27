72ccca8cf41446d35fba559f169591388226f056
CynthiaBorotPNV <cynthia.borot@vanoise-parcnational.fr>
2026-05-27 10:49:50 +0200

    feat: Delete some logs on devices services

--- frontend/app/services/devices.service.ts
+++ frontend/app/services/devices.service.ts
@@ -22,7 +22,6 @@ export class DevicesService {
 
   getDevices(params: DevicesAPIParams = {}): Observable<PaginatedItemCollection<Device>> {
     let httpParams = new HttpParams();
-    console.log('Initial params :', params);
     params.page ??= 1
     params.limit ??= DATA_TABLE_CONFIG.PER_PAGE_OPTION  
 
@@ -33,7 +32,6 @@ export class DevicesService {
       }
     });
 
-    console.log('GET request on :', `${this.OBJECT_API}`, 'with params :', httpParams);
     return this._http.get<PaginatedItemCollection<Device>>(
       `${this.OBJECT_API}`, { params: httpParams }
     );
