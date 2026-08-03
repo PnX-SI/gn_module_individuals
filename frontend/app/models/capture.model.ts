93bfd5b61e47a326a8c8d6814d78c111a0900cbf
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-08-03 17:15:05 +0200

    chore: add model

---
+++ frontend/app/models/capture.model.ts
@@ -0,0 +1,20 @@
+import { Individual } from './individuals.models';
+
+export interface GeoJSON {}
+
+export interface IndividualCaptured {
+  individual: Individual;
+  additional_data: any;
+}
+
+export interface Capture {
+  id_nomenclature_protocole: number;
+  comment: string;
+  date: Date;
+  geom: any;
+  id_digitiser: number;
+  meta_create_date: Date;
+  meta_update_date: Date;
+  observers: any[];
+  individuals: IndividualCaptured[];
+}
